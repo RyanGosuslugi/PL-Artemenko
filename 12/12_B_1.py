3f623dd7721fbfb2ffc030683178c13827197938
RyanGosuslugi <nikitaart7580@gmail.com>
2024-12-02 21:03:51 +0300

    Update 12_B_1.py

--- 12/12_B_1.py
+++ 12/12_B_1.py
@@ -1,12 +1,9 @@
 def find_max():
     n=int(input("Введите число: "))
-   
     if n==0:
         return 0
-    
-    max_in_rest=find_max()
-    
-    return max(n, max_in_rest)
+    mx=find_max()
+    return max(n, mx)
 
 
 print("Наибольшее число в последовательности:", find_max())
